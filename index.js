cf760d8f734d16aef2487e41e021a89d89c73ab8
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2019-09-11 21:51:13 -0400

    Add lambda router utils

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 module.exports = {
   connectors: require('./lib/connectors'),
   lambdaRouters: require('./lib/lambda-routers'),
+  logger: require('./lib/logger'),
   models: require('./lib/models'),
 };
