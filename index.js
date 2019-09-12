19cd53f18b470f01b3ca1c02565e8c510ac3b4b0
Patrick Bacon-Blaber <pbacon-blaber@virtru.com>
2019-09-11 22:44:31 -0400

    Add constants

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   connectors: require('./lib/connectors'),
+  constants: require('./lib/constants'),
   lambdaRouters: require('./lib/lambda-routers'),
   Logger: require('./lib/logger'),
   models: require('./lib/models'),
